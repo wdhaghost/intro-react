4b9cb559cf323f8a6fd1c592c07d028499826bff
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-04-29 11:57:48 +0200

    Gestion des listes

--- src/Components/DataManagement/Datamanagement.tsx
+++ src/Components/DataManagement/Datamanagement.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Student from "../../Models/Student";
+import SecondaryComponent from "./SecondaryComponent";
 import { CopyBlock, dracula } from "react-code-blocks";
 import CodeToShow from "./CodeToShow";
 function DataManagement() {
@@ -12,15 +13,34 @@ function DataManagement() {
     Firstname:"Jotaro",
     Class:"A3 Dev"
   }
+
+  var StudentsList: Array<Student>=[
+    {
+      Lastname:"Wayne",
+      Firstname:"John",
+      Class:"LA"
+    },
+    {
+      Lastname:"Payne",
+      Firstname:"Max",
+      Class:"NYPD"
+    }
+  ]
+
+  const NypdCop: Array<Student> = StudentsList.filter((Student)=>Student.Class==="LA")
   return (
     <div>
       <h2>Gestion de la donnée</h2>
 
       <div className="description">
         Utiliser des accolades pour afficher de la data
-        <p>{StudentTs.Firstname} {StudentTs.Lastname} {StudentTs.Class}</p>
+      
       </div>
       <hr />
+      {NypdCop.map((Student)=>
+        <SecondaryComponent Lastname={Student.Lastname} Firstname={Student.Firstname} Class={Student.Class} />
+
+      )}
       <div className="codeBlock">
         <CopyBlock
           language="tsx"
