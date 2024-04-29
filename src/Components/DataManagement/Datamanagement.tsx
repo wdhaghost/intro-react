ba4b2601472d0dc0085ad4707b9087b954da6f1d
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-04-29 11:00:55 +0200

    Class Student created

--- src/Components/DataManagement/Datamanagement.tsx
+++ src/Components/DataManagement/Datamanagement.tsx
@@ -1,26 +1,24 @@
 import * as React from "react";
+import Student from "../../Models/Student";
 import { CopyBlock, dracula } from "react-code-blocks";
 import CodeToShow from "./CodeToShow";
 function DataManagement() {
   const codeToShow = CodeToShow;
+  var nombre :number = 400
+  var text :string =""
+
+  const StudentTs :Student ={
+    Lastname:"Kujo",
+    Firstname:"Jotaro",
+    Class:"A3 Dev"
+  }
   return (
     <div>
       <h2>Gestion de la donnée</h2>
 
       <div className="description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sed
-        ullam hic non. Rem doloremque optio veritatis hic ducimus nobis maiores,
-        incidunt quis, quidem, beatae obcaecati eligendi deserunt qui velit! Qui
-        laudantium temporibus, illo obcaecati ratione nihil nam ipsum veritatis
-        ullam, amet natus, nisi necessitatibus suscipit illum. Dolore, odio est!
-        Est minima fugit vitae eaque similique voluptatibus et dolore amet?
-        Sapiente incidunt unde doloremque aperiam corporis quas, natus delectus
-        voluptatem perferendis saepe atque consequuntur nulla expedita magni,
-        sunt eligendi possimus est praesentium fugiat hic voluptas qui!
-        Explicabo tenetur tempora consequatur. Dolores adipisci ratione rem.
-        Consequatur praesentium blanditiis quis dolorum modi, vero veniam ex hic
-        perspiciatis nesciunt in maxime ratione sunt pariatur! Aut quia
-        consequuntur tempore odio rem magni ducimus reiciendis.
+        Utiliser des accolades pour afficher de la data
+        <p>{StudentTs.Firstname} {StudentTs.Lastname} {StudentTs.Class}</p>
       </div>
       <hr />
       <div className="codeBlock">
